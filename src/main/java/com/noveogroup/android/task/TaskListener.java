2b036dcb990552f19546b0fd3eaa7c7bd9fbeb6c
Pavel Stepanov <stefan.nsk@gmail.com>
2013-02-21 12:10:04 +0700

    write JavaDoc

--- src/main/java/com/noveogroup/android/task/TaskListener.java
+++ src/main/java/com/noveogroup/android/task/TaskListener.java
@@ -26,56 +26,8 @@
 
 package com.noveogroup.android.task;
 
-/* todo onQueueInsert и onQueueRemove - это какая-то ересz
-эти операции должны быть мгновенны.
-все пошло от желания иметь гарантированно вызываемые onCreate-onDestroy
-а при добавлении задачи в отмененную очередь у меня жизненный цикл шел вообще не так
-то есть есть необходимость - не вызывать кучу раз листенеры и не дергать
-progress dialog для по-любому отмененных задач.
- */
-////////////////////////////////////////////////////////////////////////////////
-// * Методы при создании задачи:      onCreate      - onDestroy
-// * Методы при добавлении в очередь: onQueueInsert - onQueueRemove
-// * Методы при исполнении задачи:    onStart       - onFinish
-// * Задача отменена:   onCanceled
-// * Задача поломалась: onFailed
-// * Задача выполнена:  onSucceed
-////////////////////////////////////////////////////////////////////////////////
-// Порядок исполнения листенеров подобран специально - сначала прямой, а в конце
-// обратный порядку добаления. Это сделано для симметрии исполнения.
-////////////////////////////////////////////////////////////////////////////////
-// * Создали задачу и добавили в очередь
-//   > CREATE onCreate                        { in direct order
-// * Прервали задачу пока она была в очереди
-//   > проставляется флаг interrupted
-//   > CANCELED onCanceled onDestroy          } in reverse order
-// * Задача поступила на исполнение
-//   > STARTED onStart                        { in direct order
-// * Задачу пытаются прервать во время исполнения
-//   > проставляется флаг interrupted
-//   > ничего не происходит
-// * Задача корректно завершилась
-//   > SUCCEED onFinish onSucceed onDestroy   } in reverse order
-// * Задача выбросила throwable (в том числе и InterruptedException)
-//   > проставляется throwable
-//   > FAILED onFinish onFailed onDestroy     } in reverse order
-// * Задачу создали в убитом executor-е:
-//   > CANCELED onCanceled
-////////////////////////////////////////////////////////////////////////////////
-// Таким образом возможны следующие последовательности:
-// CREATE [ADD] onCreate CANCELED onCanceled [DEL] onDestroy
-// CREATE [ADD] onCreate STARTED onStart [RUN] SUCCEED onFinish onSucceed [DEL] onDestroy
-// CREATE [ADD] onCreate STARTED onStart [RUN] FAILED  onFinish onFailed  [DEL] onDestroy
-// CANCELED onCanceled
-////////////////////////////////////////////////////////////////////////////////
-
 /**
  * Interface definition for a callbacks to be invoked during task lifecycle.
- * <p/>
- * Each task can have a set of listeners to report its state to. When it is
- * needed the corresponding callback methods (the same for each listener) are
- * called is direct or reverse order, one by one. The task won't call a next
- * set of callbacks before the current is done.
  * <p/>
  * The callback method can take any time to execute - it only makes the task
  * lifetime longer. If some exception is thrown from a callback it won't be
@@ -100,9 +52,58 @@ progress dialog для по-любому отмененных задач.
  * </ul>
  * </li>
  * </ul>
+ * Each task can have a set of listeners to report its state to. When it is
+ * needed the corresponding callback methods (the same for each listener) are
+ * called is direct or reverse order, one by one. The task won't call a next
+ * set of callbacks before the current is done.
+ * <p/>
+ * Any process of task execution passes through the following steps:
+ * <ol>
+ * <li>User calls one of methods that start a task execution.
+ * {@link TaskExecutor} creates {@link TaskHandler} object to handle new
+ * task, check if it can be executed ({@link TaskExecutor} isn't shutdown and
+ * owner {@link TaskSet} isn't interrupted) and adds it to to queue if so.
+ * {@link TaskExecutor} sets {@link TaskHandler.State#CREATED} as a state of
+ * execution processes. After that {@link TaskExecutor} prepares callbacks of
+ * listeners of just created task to be executed in separate thread.</li>
+ * <li>It is possible that the user manages to interrupt the task before
+ * the callbacks are executed. This case is absolutely the same as if the user
+ * tried to execute task on shutdown {@link TaskExecutor} or if owner
+ * {@link TaskSet} had been already interrupted. In this cases the task will be
+ * removed from queue, state will be set to {@link TaskHandler.State#CANCELED}
+ * and an interruption request will be sent.</li>
+ * <li>When one of background working threads is ready to execute task
+ * callbacks it check if the task was already interrupted. If the task is
+ * interrupted (so it is not in the queue) the thread executes
+ * {@link #onCreate(TaskHandler)}, {@link #onCanceled(TaskHandler)},
+ * {@link #onDestroy(TaskHandler)} and finishes task processing.</li>
+ * <li>If the task hasn't been interrupted the working thread executes
+ * {@link #onCreate(TaskHandler)}, {@link #onQueueInsert(TaskHandler)} so
+ * task becomes prepared and starts to wait to be executed.</li>
+ * <li>If user cancels the task while it is waiting it will be removed from
+ * the queue, its state will be set to {@link TaskHandler.State#CANCELED} and
+ * {@link #onCanceled(TaskHandler)}, {@link #onQueueRemove(TaskHandler)},
+ * {@link #onDestroy(TaskHandler)} will be executed in a separate thread.
+ * Finally, the task processing will be finished.</li>
+ * <li>If user doesn't cancel the task it will be executed after some time.
+ * Initially, working thread sets state to {@link TaskHandler.State#STARTED}
+ * and call {@link #onStart(TaskHandler)}. After that thread calls
+ * {@link Task#run(TaskEnvironment)}.</li>
+ * <li>If an exception (even {@link InterruptedException}) is thrown from
+ * {@link Task#run(TaskEnvironment)} during execution - the task becomes
+ * {@link TaskHandler.State#FAILED}, it will be removed from queue and
+ * {@link #onFinish(TaskHandler)}, {@link #onFailed(TaskHandler)},
+ * {@link #onQueueRemove(TaskHandler)}, {@link #onDestroy(TaskHandler)}
+ * will be called.</li>
+ * <li>If no exceptions are thrown during execution - the task becomes
+ * {@link TaskHandler.State#SUCCEED}, it will be removed from queue and
+ * {@link #onFinish(TaskHandler)}, {@link #onSucceed(TaskHandler)},
+ * {@link #onQueueRemove(TaskHandler)}, {@link #onDestroy(TaskHandler)}
+ * will be called.</li>
+ * </ol>
  *
  * @see TaskListener.Default
- * @see com.noveogroup.android.task.TaskHandler.State
+ * @see TaskHandler.State
  */
 public interface TaskListener {
 
@@ -160,22 +161,73 @@ public interface TaskListener {
 
     }
 
+    /**
+     * Should be called in <b>direct</b> order.
+     *
+     * @param handler the task handler.
+     * @see #onDestroy(TaskHandler)
+     */
     public void onCreate(TaskHandler<?, ?> handler);
 
+    /**
+     * Should be called in <b>direct</b> order.
+     *
+     * @param handler the task handler.
+     * @see #onQueueRemove(TaskHandler)
+     */
     public void onQueueInsert(TaskHandler<?, ?> handler);
 
+    /**
+     * Should be called in <b>direct</b> order.
+     *
+     * @param handler the task handler.
+     * @see #onFinish(TaskHandler)
+     */
     public void onStart(TaskHandler<?, ?> handler);
 
+    /**
+     * Should be called in <b>reverse</b> order.
+     *
+     * @param handler the task handler.
+     * @see #onStart(TaskHandler)
+     */
     public void onFinish(TaskHandler<?, ?> handler);
 
+    /**
+     * Should be called in <b>reverse</b> order.
+     *
+     * @param handler the task handler.
+     * @see #onQueueInsert(TaskHandler)
+     */
     public void onQueueRemove(TaskHandler<?, ?> handler);
 
+    /**
+     * Should be called in <b>reverse</b> order.
+     *
+     * @param handler the task handler.
+     * @see #onCreate(TaskHandler)
+     */
     public void onDestroy(TaskHandler<?, ?> handler);
 
+    /**
+     * Should be called in <b>reverse</b> order.
+     *
+     * @param handler the task handler.
+     */
     public void onCanceled(TaskHandler<?, ?> handler);
 
+    /**
+     * Should be called in <b>reverse</b> order.
+     *
+     * @param handler the task handler.
+     */
     public void onFailed(TaskHandler<?, ?> handler);
 
+    /**
+     * Should be called in <b>reverse</b> order.
+     *
+     * @param handler the task handler.
+     */
     public void onSucceed(TaskHandler<?, ?> handler);
 
 }
