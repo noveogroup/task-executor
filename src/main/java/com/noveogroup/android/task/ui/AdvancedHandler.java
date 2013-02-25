1db25da281bb2721e755f3b03f5e2dc1fdbf7e3a
Pavel Stepanov <stefan.nsk@gmail.com>
2013-02-25 23:22:27 +0700

    move Pair & AssociationSet into AdvancedHandler

--- src/main/java/com/noveogroup/android/task/ui/AdvancedHandler.java
+++ src/main/java/com/noveogroup/android/task/ui/AdvancedHandler.java
@@ -30,7 +30,7 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 
-import java.util.Set;
+import java.util.*;
 
 /**
  * An AdvancedHandler allows you to process Runnable objects using usual Handler.
@@ -46,6 +46,114 @@ import java.util.Set;
 // todo review & rename
 // todo add AdvancedHandler to task environment
 public class AdvancedHandler {
+
+    /**
+     * An association set.
+     *
+     * @param <V> a type of the values.
+     */
+    private static class AssociationSet<V> {
+
+        private final Set<V> values = new HashSet<V>();
+        private final Map<Object, Set<V>> associations = new HashMap<Object, Set<V>>();
+
+        /**
+         * Adds a value to the set.
+         *
+         * @param value the value.
+         */
+        public void add(V value) {
+            values.add(value);
+        }
+
+        /**
+         * Removes a value from the set.
+         *
+         * @param value the value.
+         * @return true if the set contains the value and it is successfully removed.
+         */
+        public boolean remove(V value) {
+            for (Iterator<Object> i = associations.keySet().iterator(); i.hasNext(); ) {
+                Object key = i.next();
+                Set<V> set = associations.get(key);
+                set.remove(value);
+                if (set.size() <= 0) {
+                    i.remove();
+                }
+            }
+
+            return values.remove(value);
+        }
+
+        /**
+         * Associates a value with keys.
+         *
+         * @param value the value.
+         * @param keys  the keys.
+         */
+        public void associate(V value, Object... keys) {
+            for (Object key : keys) {
+                Set<V> set = associations.get(key);
+                if (set == null) {
+                    set = new HashSet<V>();
+                    associations.put(key, set);
+                }
+                set.add(value);
+            }
+        }
+
+        /**
+         * Returns a set of associated values from the set.
+         *
+         * @param key the key to find values associated with.
+         * @param <K> a type of the key.
+         * @return the set of values.
+         */
+        public <K> Set<V> getAssociated(K key) {
+            Set<V> set = associations.get(key);
+            return set == null ? new HashSet<V>() : new HashSet<V>(set);
+        }
+
+    }
+
+    /**
+     * This class is an improvement of {@link android.util.Pair} which
+     * fails when one of the values is null, unfortunately.
+     *
+     * @param <V1> a type of the first value.
+     * @param <V2> a type of the second value.
+     */
+    private static class Pair<V1, V2> {
+
+        public final V1 value1;
+        public final V2 value2;
+
+        public Pair(V1 value1, V2 value2) {
+            this.value1 = value1;
+            this.value2 = value2;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Pair pair = (Pair) o;
+
+            if (value1 != null ? !value1.equals(pair.value1) : pair.value1 != null) return false;
+            if (value2 != null ? !value2.equals(pair.value2) : pair.value2 != null) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash1 = value1 != null ? value1.hashCode() : 0;
+            int hash2 = value2 != null ? value2.hashCode() : 0;
+            return 0x1F * hash1 + hash2;
+        }
+
+    }
 
     private static class WaitCallback implements Runnable {
 
