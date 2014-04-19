1e61b69d46a0498af0718acc8cfd226bce7d0dd4
Pavel Stepanov <stefan.nsk@gmail.com>
2014-04-19 14:15:14 +0700

    implement testConstructor and testTags

--- android-task-executor/SampleApplication/src/androidTest/java/com/noveogroup/android/task/ui/NewUIHandlerTest.java
+++ android-task-executor/SampleApplication/src/androidTest/java/com/noveogroup/android/task/ui/NewUIHandlerTest.java
@@ -1,11 +1,49 @@
 package com.noveogroup.android.task.ui;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.test.AndroidTestCase;
+
+import junit.framework.Assert;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class NewUIHandlerTest extends AndroidTestCase {
 
-    public void testFirst() {
-        throw new RuntimeException();
+    private static Set<String> set(String... values) {
+        return new HashSet<String>(Arrays.asList(values));
+    }
+
+    private static final Runnable EMPTY_CALLBACK = new Runnable() {
+        @Override
+        public void run() {
+        }
+    };
+
+    public void testConstructor() {
+        NewUIHandler uiHandler1 = new NewUIHandler();
+        uiHandler1.post(EMPTY_CALLBACK);
+        NewUIHandler uiHandler2 = new NewUIHandler(getContext());
+        uiHandler2.post(EMPTY_CALLBACK);
+        NewUIHandler uiHandler3 = new NewUIHandler(Looper.myLooper());
+        uiHandler3.post(EMPTY_CALLBACK);
+        NewUIHandler uiHandler4 = new NewUIHandler(new Handler());
+        uiHandler4.post(EMPTY_CALLBACK);
+    }
+
+    public void testTags() {
+        NewUIHandler uiHandler = new NewUIHandler();
+        Assert.assertEquals(set(), uiHandler.tags());
+
+        NewUIHandler subHandler = uiHandler.sub("A", "B");
+        Assert.assertEquals(set("A", "B"), subHandler.tags());
+        Assert.assertEquals(uiHandler, subHandler.root());
+
+        NewUIHandler subSubHandler = subHandler.sub("C");
+        Assert.assertEquals(set("A", "B", "C"), subSubHandler.tags());
+        Assert.assertEquals(uiHandler, subSubHandler.root());
     }
 
 }
