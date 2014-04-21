5069a27907a0bbaf6a257b399c92e065c620ddd3
Pavel Stepanov <stefan.nsk@gmail.com>
2014-04-21 23:28:17 +0700

    integrate JUnit tests

---
+++ android-task-executor/AndroidTaskExecutor/src/test/java/com/noveogroup/android/task/TEST.java
@@ -0,0 +1,13 @@
+package com.noveogroup.android.task;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TEST {
+
+    @Test
+    public void test() {
+        Assert.assertEquals("bla", "bla");
+    }
+
+}
