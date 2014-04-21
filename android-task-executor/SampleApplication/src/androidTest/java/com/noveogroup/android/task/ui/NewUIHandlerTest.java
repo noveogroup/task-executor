a0b53f98084405c924e4a1e3c62ffc454e08024e
Pavel Stepanov <stefan.nsk@gmail.com>
2014-04-21 13:36:52 +0700

    implement testPost*()

--- android-task-executor/SampleApplication/src/androidTest/java/com/noveogroup/android/task/ui/NewUIHandlerTest.java
+++ android-task-executor/SampleApplication/src/androidTest/java/com/noveogroup/android/task/ui/NewUIHandlerTest.java
@@ -12,8 +12,39 @@ import java.util.Set;
 
 public class NewUIHandlerTest extends AndroidTestCase {
 
+    private static final long DT = 10;
+
     private static Set<String> set(String... values) {
         return new HashSet<String>(Arrays.asList(values));
+    }
+
+    private static void sleep(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException ignored) {
+        }
+    }
+
+    public static void run(final Runnable runnable) {
+        final RuntimeException[] exception = new RuntimeException[1];
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    runnable.run();
+                } catch (RuntimeException e) {
+                    exception[0] = e;
+                }
+            }
+        };
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException ignored) {
+        }
+        if (exception[0] != null) {
+            throw exception[0];
+        }
     }
 
     public void testConstructor() {
@@ -34,6 +65,86 @@ public class NewUIHandlerTest extends AndroidTestCase {
         NewUIHandler subSubHandler = subHandler.sub("C");
         Assert.assertEquals(set("A", "B", "C"), subSubHandler.tags());
         Assert.assertEquals(uiHandler, subSubHandler.root());
+    }
+
+    public void testPost1() {
+        run(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+                NewUIHandler uiHandler = new NewUIHandler(getContext());
+
+                uiHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        builder.append("callback");
+                    }
+                });
+
+                sleep(DT);
+
+                Assert.assertEquals("callback", builder.toString());
+            }
+        });
+    }
+
+    public void testPost2() {
+        run(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+                NewUIHandler uiHandler = new NewUIHandler(getContext());
+
+                uiHandler.post(10 * DT, new Runnable() {
+                    @Override
+                    public void run() {
+                        builder.append("callback");
+                    }
+                });
+
+                sleep(DT);
+
+                Assert.assertEquals("", builder.toString());
+            }
+        });
+    }
+
+    public void testPost3() {
+        run(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+                NewUIHandler uiHandler = new NewUIHandler(getContext());
+
+                uiHandler.sub("A").post(10 * DT, new Runnable() {
+                    @Override
+                    public void run() {
+                        builder.append("[callback-A]");
+                    }
+                });
+
+                uiHandler.sub("B").post(DT, new Runnable() {
+                    @Override
+                    public void run() {
+                        builder.append("[callback-B]");
+                    }
+                });
+
+                uiHandler.sub("C").post(new Runnable() {
+                    @Override
+                    public void run() {
+                        builder.append("[callback-C]");
+                    }
+                });
+
+                sleep(5 * DT);
+
+                Assert.assertEquals("[callback-C][callback-B]", builder.toString());
+            }
+        });
     }
 
 }
