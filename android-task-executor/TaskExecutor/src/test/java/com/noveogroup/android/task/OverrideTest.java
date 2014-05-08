fb27dce62fdcddca6ebe14b5d281e0c898e6d43f
Pavel Stepanov <stefan.nsk@gmail.com>
2014-05-09 02:18:29 +0700

    implement OverrideTest (without generics)

--- android-task-executor/TaskExecutor/src/test/java/com/noveogroup/android/task/OverrideTest.java
+++ android-task-executor/TaskExecutor/src/test/java/com/noveogroup/android/task/OverrideTest.java
@@ -3,93 +3,321 @@ package com.noveogroup.android.task;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class OverrideTest {
 
-    public static class LogEnvironment<E extends LogEnvironment> extends SimpleTaskEnvironment<LogEnvironment<E>> {
-        private final StringBuffer log;
+    private static final String ARG_LOG = "log";
 
-        public LogEnvironment(TaskHandler<?, LogEnvironment<E>> handler, StringBuffer log) {
+    private static synchronized void log(Pack pack, String place, Object... args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        builder.append(place);
+        for (Object arg : args) {
+            Class<?> aClass = arg.getClass();
+            String name;
+            if (aClass.isAnonymousClass()) {
+                if (aClass.getSuperclass() == Object.class) {
+                    name = aClass.getInterfaces()[0].getName() + "{...}";
+                } else {
+                    name = aClass.getSuperclass().getName() + "{...}";
+                }
+            } else {
+                name = aClass.getName();
+            }
+            name = name.replaceAll("com.noveogroup.android.task.", "");
+            name = name.replaceAll("OverrideTest\\$", "\\$");
+            builder.append("|").append(name);
+        }
+        builder.append("]\n");
+        pack.<StringBuffer>get(ARG_LOG).append(builder.toString());
+    }
+
+    public static class TaskEnvironment1 extends SimpleTaskEnvironment {
+        public TaskEnvironment1(TaskHandler handler) {
             super(handler);
-            this.log = log;
-        }
-
-        public void log(String message) {
-            log.append(message);
         }
     }
 
-    public static class LogExecutor<E extends LogEnvironment> extends SimpleTaskExecutor<LogEnvironment<E>> {
-        protected final StringBuffer log = new StringBuffer();
+    public static class TaskEnvironment2 extends TaskEnvironment1 {
+        public TaskEnvironment2(TaskHandler handler) {
+            super(handler);
+        }
+    }
 
-        public LogExecutor(ExecutorService executorService) {
+    public static class TaskExecutor1 extends SimpleTaskExecutor {
+        public TaskExecutor1(ExecutorService executorService) {
             super(executorService);
         }
 
         @Override
-        protected <T extends Task> LogEnvironment<E> createTaskEnvironment(TaskHandler<T, LogEnvironment<E>> taskHandler) {
-            return new LogEnvironment<E>(taskHandler, log);
-        }
-
-        public String getLog() {
-            return log.toString();
+        protected TaskEnvironment1 createTaskEnvironment(TaskHandler taskHandler) {
+            return new TaskEnvironment1(taskHandler);
         }
     }
 
-    public static class CustomEnvironment<E extends CustomEnvironment> extends LogEnvironment<CustomEnvironment<E>> {
-        public CustomEnvironment(TaskHandler<?, LogEnvironment<CustomEnvironment<E>>> handler, StringBuffer log) {
-            super(handler, log);
-        }
-
-        public void customMethod() {
-            log("[CustomEnvironment::customMethod()]");
-        }
-    }
-
-    public static class CustomExecutor<E extends CustomEnvironment> extends LogExecutor<CustomEnvironment<E>> {
-        public CustomExecutor(ExecutorService executorService) {
+    public static class TaskExecutor2 extends TaskExecutor1 {
+        public TaskExecutor2(ExecutorService executorService) {
             super(executorService);
         }
 
         @Override
-        protected <T extends Task> LogEnvironment<CustomEnvironment<E>> createTaskEnvironment(TaskHandler<T, LogEnvironment<CustomEnvironment<E>>> taskHandler) {
-            return new CustomEnvironment<E>(taskHandler, log);
+        protected TaskEnvironment2 createTaskEnvironment(TaskHandler taskHandler) {
+            return new TaskEnvironment2(taskHandler);
+        }
+    }
+
+    public static interface Task1 extends Task {
+    }
+
+    public static interface Task2 extends Task1 {
+    }
+
+    public static class MyTask1 implements Task1 {
+        @Override
+        public void run(TaskEnvironment env) throws Throwable {
+            TaskEnvironment1 taskEnvironment1 = (TaskEnvironment1) env;
+            log(env.args(), "MyTask1::run", taskEnvironment1);
+        }
+    }
+
+    public static class MyTask2 implements Task2 {
+        @Override
+        public void run(TaskEnvironment env) throws Throwable {
+            TaskEnvironment2 taskEnvironment2 = (TaskEnvironment2) env;
+            log(env.args(), "MyTask2::run", taskEnvironment2);
+        }
+    }
+
+    public static class MyTaskListener1 extends TaskListener.Default {
+        @Override
+        public void onCreate(TaskHandler handler) {
+            TaskExecutor1 taskExecutor1 = (TaskExecutor1) handler.executor();
+            Task1 task1 = (Task1) handler.task();
+            log(handler.args(), "MyTaskListener1::onCreate", task1, taskExecutor1);
         }
 
-        public void customMethod() {
-            log.append("[CustomExecutor::customMethod()]");
+        @Override
+        public void onDestroy(TaskHandler handler) {
+            Task1 task1 = (Task1) handler.task();
+            TaskExecutor1 taskExecutor1 = (TaskExecutor1) handler.executor();
+            log(handler.args(), "MyTaskListener1::onDestroy", task1, taskExecutor1);
+        }
+    }
+
+    public static class MyTaskListener2 extends TaskListener.Default {
+        @Override
+        public void onCreate(TaskHandler handler) {
+            Task2 task2 = (Task2) handler.task();
+            TaskExecutor2 taskExecutor2 = (TaskExecutor2) handler.executor();
+            log(handler.args(), "MyTaskListener2::onCreate", task2, taskExecutor2);
+        }
+
+        @Override
+        public void onDestroy(TaskHandler handler) {
+            Task2 task2 = (Task2) handler.task();
+            TaskExecutor2 taskExecutor2 = (TaskExecutor2) handler.executor();
+            log(handler.args(), "MyTaskListener2::onDestroy", task2, taskExecutor2);
+        }
+    }
+
+    /* ******************************************************************************** */
+
+    private static void checkHandler(TaskHandler taskHandler) throws Throwable {
+        Throwable throwable = taskHandler.getThrowable();
+        if (throwable != null) {
+            throw throwable;
         }
     }
 
     @Test
-    public void logExecutorTest() throws InterruptedException {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        LogExecutor<LogEnvironment> logExecutor = new LogExecutor<LogEnvironment>(executorService);
-        logExecutor.execute(new Task<LogEnvironment<LogEnvironment>>() {
+    public void taskExecutor1AnonymousTask() throws Throwable {
+        TaskExecutor1 executor1 = new TaskExecutor1(Executors.newSingleThreadExecutor());
+
+        StringBuffer log = new StringBuffer();
+        Pack args = new Pack().put(ARG_LOG, log);
+
+        TaskHandler handler = executor1.execute(new Task() {
             @Override
-            public void run(LogEnvironment<LogEnvironment> env) throws Throwable {
-                env.log("[Task::run()]");
+            public void run(TaskEnvironment env) throws Throwable {
+                log(env.args(), "Task::run", env);
             }
-        }).join();
-        Assert.assertEquals("[Task::run()]", logExecutor.getLog());
+        }, args, new TaskListener.Default() {
+            @Override
+            public void onCreate(TaskHandler handler) {
+                Task task = handler.task();
+                TaskExecutor taskExecutor = handler.executor();
+                log(handler.args(), "TaskListener::onCreate", task, taskExecutor);
+            }
+
+            @Override
+            public void onDestroy(TaskHandler handler) {
+                Task task = handler.task();
+                TaskExecutor taskExecutor = handler.executor();
+                log(handler.args(), "TaskListener::onDestroy", task, taskExecutor);
+            }
+        });
+        handler.join();
+
+        Task task = handler.task();
+        TaskExecutor executor = handler.executor();
+        log(handler.args(), "1", task, executor);
+        checkHandler(handler);
+
+
+        Assert.assertEquals("" +
+                        "[TaskListener::onCreate|Task{...}|$TaskExecutor1]\n" +
+                        "[Task::run|$TaskEnvironment1]\n" +
+                        "[TaskListener::onDestroy|Task{...}|$TaskExecutor1]\n" +
+                        "[1|Task{...}|$TaskExecutor1]\n",
+                log.toString()
+        );
     }
 
     @Test
-    public void customExecutorTest() throws InterruptedException {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        CustomExecutor<CustomEnvironment> customExecutor = new CustomExecutor<CustomEnvironment>(executorService);
-        customExecutor.execute(new Task<LogEnvironment<CustomEnvironment<CustomEnvironment>>>() {
+    public void taskExecutor1AnonymousTask1() throws Throwable {
+        TaskExecutor1 executor1 = new TaskExecutor1(Executors.newSingleThreadExecutor());
+
+        StringBuffer log = new StringBuffer();
+        Pack args = new Pack().put(ARG_LOG, log);
+
+        TaskHandler handler = executor1.execute(new Task1() {
             @Override
-            public void run(LogEnvironment<CustomEnvironment<CustomEnvironment>> env) throws Throwable {
-                // todo strange
-                ((CustomEnvironment<CustomEnvironment>) env).customMethod();
-                env.log("[Task::run()]");
+            public void run(TaskEnvironment env) throws Throwable {
+                log(env.args(), "Task1::run", (TaskEnvironment1) env);
             }
-        }).join();
-        customExecutor.customMethod();
-        Assert.assertEquals("[CustomEnvironment::customMethod()][Task::run()][CustomExecutor::customMethod()]", customExecutor.getLog());
+        }, args, Arrays.<TaskListener>asList(new TaskListener.Default() {
+            @Override
+            public void onCreate(TaskHandler handler) {
+                Task1 task1 = (Task1) handler.task();
+                TaskExecutor1 taskExecutor1 = (TaskExecutor1) handler.executor();
+                log(handler.args(), "TaskListener::onCreate", task1, taskExecutor1);
+            }
+
+            @Override
+            public void onDestroy(TaskHandler handler) {
+                Task1 task1 = (Task1) handler.task();
+                TaskExecutor1 taskExecutor1 = (TaskExecutor1) handler.executor();
+                log(handler.args(), "TaskListener::onDestroy", task1, taskExecutor1);
+            }
+        }, new MyTaskListener1()));
+        handler.join();
+
+        Task1 task1 = (Task1) handler.task();
+        TaskExecutor1 taskExecutor1 = (TaskExecutor1) handler.executor();
+        log(handler.args(), "2", task1, taskExecutor1);
+        checkHandler(handler);
+
+        Assert.assertEquals("" +
+                        "[TaskListener::onCreate|$Task1{...}|$TaskExecutor1]\n" +
+                        "[MyTaskListener1::onCreate|$Task1{...}|$TaskExecutor1]\n" +
+                        "[Task1::run|$TaskEnvironment1]\n" +
+                        "[MyTaskListener1::onDestroy|$Task1{...}|$TaskExecutor1]\n" +
+                        "[TaskListener::onDestroy|$Task1{...}|$TaskExecutor1]\n" +
+                        "[2|$Task1{...}|$TaskExecutor1]\n",
+                log.toString()
+        );
+    }
+
+    @Test
+    public void taskExecutor1AnonymousTask2() throws Throwable {
+        TaskExecutor1 executor1 = new TaskExecutor1(Executors.newSingleThreadExecutor());
+
+        StringBuffer log = new StringBuffer();
+        Pack args = new Pack().put(ARG_LOG, log);
+
+        TaskHandler handler = executor1.execute(new Task2() {
+            @Override
+            public void run(TaskEnvironment env) throws Throwable {
+                log(env.args(), "Task2::run", (TaskEnvironment2) env);
+            }
+        }, args, Arrays.<TaskListener>asList(new TaskListener.Default() {
+            @Override
+            public void onCreate(TaskHandler handler) {
+                Task1 task1 = (Task1) handler.task();
+                TaskExecutor1 taskExecutor1 = (TaskExecutor1) handler.executor();
+                log(handler.args(), "TaskListener::onCreate", task1, taskExecutor1);
+            }
+
+            @Override
+            public void onDestroy(TaskHandler handler) {
+                Task1 task1 = (Task1) handler.task();
+                TaskExecutor1 taskExecutor1 = (TaskExecutor1) handler.executor();
+                log(handler.args(), "TaskListener::onDestroy", task1, taskExecutor1);
+            }
+        }, new TaskListener.Default() {
+            @Override
+            public void onCreate(TaskHandler handler) {
+                Task2 task2 = (Task2) handler.task();
+                TaskExecutor2 taskExecutor2 = (TaskExecutor2) handler.executor();
+                log(handler.args(), "TaskListener::onCreate", task2, taskExecutor2);
+            }
+
+            @Override
+            public void onDestroy(TaskHandler handler) {
+                Task2 task2 = (Task2) handler.task();
+                TaskExecutor2 taskExecutor2 = (TaskExecutor2) handler.executor();
+                log(handler.args(), "TaskListener::onDestroy", task2, taskExecutor2);
+            }
+        }, new MyTaskListener1()));
+        handler.join();
+
+        Task1 task1 = (Task1) handler.task();
+        TaskExecutor1 taskExecutor1 = (TaskExecutor1) handler.executor();
+        log(handler.args(), "2", task1, taskExecutor1);
+        checkHandler(handler);
+
+        Assert.assertEquals("" +
+                        "[TaskListener::onCreate|$Task1{...}|$TaskExecutor1]\n" +
+                        "[MyTaskListener1::onCreate|$Task1{...}|$TaskExecutor1]\n" +
+                        "[Task1::run|$TaskEnvironment1]\n" +
+                        "[MyTaskListener1::onDestroy|$Task1{...}|$TaskExecutor1]\n" +
+                        "[TaskListener::onDestroy|$Task1{...}|$TaskExecutor1]\n" +
+                        "[2|$Task1{...}|$TaskExecutor1]\n",
+                log.toString()
+        );
+    }
+
+    @Test
+    public void taskExecutor1MyTask1() throws Throwable {
+        TaskExecutor1 executor1 = new TaskExecutor1(Executors.newSingleThreadExecutor());
+
+        StringBuffer log = new StringBuffer();
+        Pack args = new Pack().put(ARG_LOG, log);
+
+        TaskHandler handler = executor1.execute(new MyTask1(), args, Arrays.<TaskListener>asList(new TaskListener.Default() {
+            @Override
+            public void onCreate(TaskHandler handler) {
+                Task1 task1 = (Task1) handler.task();
+                TaskExecutor1 taskExecutor1 = (TaskExecutor1) handler.executor();
+                log(handler.args(), "TaskListener::onCreate", task1, taskExecutor1);
+            }
+
+            @Override
+            public void onDestroy(TaskHandler handler) {
+                Task1 task1 = (Task1) handler.task();
+                TaskExecutor1 taskExecutor1 = (TaskExecutor1) handler.executor();
+                log(handler.args(), "TaskListener::onDestroy", task1, taskExecutor1);
+            }
+        }, new MyTaskListener1()));
+        handler.join();
+
+        MyTask1 myTask1 = (MyTask1) handler.task();
+        TaskExecutor1 taskExecutor1 = (TaskExecutor1) handler.executor();
+        log(handler.args(), "3", myTask1, taskExecutor1);
+        checkHandler(handler);
+
+        Assert.assertEquals("" +
+                        "[TaskListener::onCreate|$MyTask1|$TaskExecutor1]\n" +
+                        "[MyTaskListener1::onCreate|$MyTask1|$TaskExecutor1]\n" +
+                        "[MyTask1::run|$TaskEnvironment1]\n" +
+                        "[MyTaskListener1::onDestroy|$MyTask1|$TaskExecutor1]\n" +
+                        "[TaskListener::onDestroy|$MyTask1|$TaskExecutor1]\n" +
+                        "[3|$MyTask1|$TaskExecutor1]\n",
+                log.toString()
+        );
     }
 
 }
